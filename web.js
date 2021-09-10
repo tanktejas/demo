ea5857cc51200d1c141e5940c85a68de31b4f327
tanktejas <tejastank20001@gmail.com>
2021-09-10 23:28:52 +0530

    initial commit

---
+++ web.js
@@ -0,0 +1,9 @@
+const showmenu = () => {
+    document.querySelector('.navlink').style.display = "block";
+    
+
+}
+const hidemenu = () => {
+    document.querySelector('.navlink').style.display = "none";
+}
+         
